43bde38c18ae7c82de6bfd26229cf9429be7e58d
ignaciovacca <vaccaignacio@gmail.com>
2020-03-29 14:12:03 -0300

    Ejercio 3 resuelto

---
+++ practica-1/3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# 3. Desarrolle un script que guarde en un arreglo todos los archivos del directorio actual (in-
+# cluyendo sus subdirectorios) para los cuales el usuario que ejecuta el script tiene permisos
+# de ejecución. Luego, implemente las siguientes funciones:
+# 	(a) cantidad: Imprime la cantidad de archivos que se encontraron
+#	(b) archivos: Imprime los nombres de los archivos encontrados en orden alfabético
+
+files=()
+
+for file in `find $PWD -type f -executable`
+do
+	files=(${files[*]} $file)
+done
+
+function cantidad() {
+	echo "Cantidad de archivos: ${#files[@]};"
+}
+
+function archivos() {
+	echo "Archivos en orden alfabetico"
+	for a in "${files[@]}";do echo "$a";done | sort
+}
+
+cantidad
+archivos
+
