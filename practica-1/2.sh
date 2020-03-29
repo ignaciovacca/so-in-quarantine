97ec8bc8667481f4966531f3e041d6a91245b9b4
ignaciovacca <vaccaignacio@gmail.com>
2020-03-29 12:24:37 -0300

    Ejercicio 2 resuelto

---
+++ practica-1/2.sh
@@ -0,0 +1,22 @@
+# 2. Implemente un script que reciba como parámetro el nombre de un proceso e informe cada
+# 15 segundos cuántas instancias de ese proceso están en ejecución.i
+
+function usage() {
+	echo "Modo de uso:"
+	echo "	2.sh nombre proceso"
+}
+
+if [ $# -lt 1 ]
+then
+	usage
+	exit -1
+fi
+
+process_name=$1
+
+for ((;;))
+do
+	instances=`ps -e | grep $process_name | wc -l`
+	echo "Instancias del proceso $process_name: $instances"
+	sleep 15
+done
